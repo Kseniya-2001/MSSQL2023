87eefe5f2dcbdf14a4f7435fd01a4bc714f9c6d1
Khaduskina Kseniya <146877307+Kseniya-2001@users.noreply.github.com>
2024-01-26 23:09:48 +0300

    ДЗ 3 к уроку 5

--- SQL_DZ0503.sql
+++ SQL_DZ0503.sql
@@ -1,6 +1,7 @@
 /*3. Выберите информацию по клиентам, которые перевели компании 5 максимальных платежей 
 из [Sales].[CustomerTransactions] представьте 3 способа (в том числе с CTE)*/
 
-select  TOP(5) *  
+select TOP(5) CustomerID, SUM(TransactionAmount) as SumTransaction 
 from [Sales].[CustomerTransactions]
-having MAX(TransactionAmount) 
+group by CustomerID  
+order by SumTransaction desc 

bfaaa7b15c3c47d473a9a76aa71f7d747be7c66d
Khaduskina Kseniya <146877307+Kseniya-2001@users.noreply.github.com>
2024-02-18 22:15:07 +0300

    Замечания исправлены

--- SQL_DZ0102.sql
+++ SQL_DZ0102.sql
@@ -1,12 +1,12 @@
-/*2. ѕоставщиков, у которых не было сделано ни одного заказа 
+/*2. поставщиков, у которых не было сделано ни одного заказа 
 (потом покажем как это делать через подзапрос, сейчас сделайте через JOIN)*/
 
-select *
+select s.SupplierID, s.[SupplierName] 
 from [Purchasing].[Suppliers] as s
 left join [Purchasing].[SupplierTransactions] as st on st.SupplierID = s.SupplierID
 where st.SupplierID is null --1 вариант
 
-select *
+select SupplierID, SupplierName 
 from [Purchasing].[Suppliers] 
 where SupplierID not in (select SupplierID
                      from [Purchasing].[SupplierTransactions]) --2 вариант

2b62305018590072842db8dedc29c8f1f0cf7ed8
Khaduskina Kseniya <146877307+Kseniya-2001@users.noreply.github.com>
2024-02-21 21:50:30 +0300

    вместо Description взяла StockItem

--- DZ01/SQL_DZ0101.sql
+++ DZ01/SQL_DZ0101.sql
@@ -1,7 +1,5 @@
  /*1. Все товары, в которых в название есть пометка urgent или название начинается с Animal*/
-
-
-      
+     
 select [StockItemID],[StockItemName] 
 from [Warehouse].[StockItems]
 where [StockItemName]  like '%urgent%' or

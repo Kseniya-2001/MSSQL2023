d5d75c8476e1fd788f1fd234b3f09af9f0438068
Khaduskina Kseniya <146877307+Kseniya-2001@users.noreply.github.com>
2024-03-14 23:16:11 +0300

    Дз 1 к уроку 2, решила вставить строки в другую таблицу, получилось

--- DZ02/SQL_DZ0201.sql
+++ DZ02/SQL_DZ0201.sql
@@ -1,61 +1,9 @@
-/*1. ƒовставл¤ть в базу 5 записей использу¤ insert в таблицу Customers или Suppliers*/
+/*1. довставлять в базу 5 записей используя insert в таблицу Customers или Suppliers*/
 
-INSERT INTO [Sales].[Customers]
-           ([CustomerName]
-           ,[BillToCustomerID]
-           ,[CustomerCategoryID]
-           ,[BuyingGroupID]
-           ,[PrimaryContactPersonID]
-           ,[AlternateContactPersonID]
-           ,[DeliveryMethodID]
-           ,[DeliveryCityID]
-           ,[PostalCityID]
-           ,[CreditLimit]
-           ,[AccountOpenedDate]
-           ,[StandardDiscountPercentage]
-           ,[IsStatementSent]
-           ,[IsOnCreditHold]
-           ,[PaymentDays]
-           ,[PhoneNumber]
-           ,[FaxNumber]
-           ,[DeliveryRun]
-           ,[RunPosition]
-           ,[WebsiteURL]
-           ,[DeliveryAddressLine1]
-           ,[DeliveryAddressLine2]
-           ,[DeliveryPostalCode]
-           ,[DeliveryLocation]
-           ,[PostalAddressLine1]
-           ,[PostalAddressLine2]
-           ,[PostalPostalCode]
-           ,[LastEditedBy])
-     VALUES
-           ('Lakshmi Benipal',
-		   401,
-           3,
-           2,
-           2225,
-           2226,
-           3,
-           3673,
-           3081,
-           NULL,
-           2013-07-18,
-           0.000,
-           0,
-           0,
-           7,
-           '(405) 278-0112',
-           '(252) 555-2708',
-           NULL,
-           NULL,
-           'http://www.microsoft.com/AnnaGyarmathi/',
-           'Suite 27',
-           '1466 Deilami Road',
-           90005,
-           '0xE6100000010CCA80FD2147114440B27790E8AF355AC0',
-           'PO Box 9499',
-           'Eddenville',
-           90423,
-           8)
-GO
+insert into [Sales].[CustomerCategories]
+            ([CustomerCategoryName], [LastEditedBy] )
+values ('Kseniya Haduskina', 1),
+       ('Vladyslav Haduskin', 1),
+	   ('Vladimir Novopashin', 1),
+	   ('Ruslan Zhumaev', 7),
+	   ('Nadezhda Morozova', 10) 
